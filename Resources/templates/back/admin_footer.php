69633f5f4ea2347f2243e0a5202236160dcf9421
John Paul <28johnpaulsim28@gmail.com>
2020-12-13 11:57:11 +0800

    fix sidebar

--- Resources/templates/back/admin_footer.php
+++ Resources/templates/back/admin_footer.php
@@ -1,8 +1,10 @@
 </div>
        <script>
-        $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggleClass('active');
-        });
+       $(document).ready(function () {
+                $('#sidebarCollapse').on('click', function () {
+                        $('#sidebar').toggleClass('active');
+                });
+       });
        </script>
        </div> 
        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
